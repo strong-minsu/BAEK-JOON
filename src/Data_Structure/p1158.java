18b5ebfd9eda3c6cc6107bbdd4ab9eb4da1095da
strong-minsu <dlalstn5789@gmail.com>
2023-10-31 23:42:26 +0900

    2023-10-31: problem solving
    Signed-off-by: strong-minsu <dlalstn5789@gmail.com>

---
+++ src/Data_Structure/p1158.java
@@ -0,0 +1,45 @@
+package Data_Structure;
+
+import java.util.*;
+import java.io.*;
+
+public class p1158 {
+
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N ; i++) {
+            queue.add(i);
+        }
+
+        int count = 0;
+        sb.append("<");
+        while (!queue.isEmpty()){
+            int number = queue.poll();
+            count++;
+
+            if(count == K){
+                if(queue.isEmpty()){
+                    sb.append(number);
+                }
+                else{
+                    sb.append(number).append(", ");
+                }
+                count = 0;
+            }
+            else{
+                queue.add(number);
+            }
+        }
+        sb.append(">");
+
+        System.out.print(sb);
+    }
+}
